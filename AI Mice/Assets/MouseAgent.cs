25d00a36eca3acd701a007e2edf77c678f944f45
mbuet2ner <20720363+mbuet2ner@users.noreply.github.com>
2019-11-03 19:04:05 +0200

    work on rotation, movement and perception

--- AI Mice/Assets/MouseAgent.cs
+++ AI Mice/Assets/MouseAgent.cs
@@ -4,22 +4,24 @@ using MLAgents;
 
 public class MouseAgent : Agent
 {
-    Rigidbody rBody;
+    public float moveSpeed = 1f;
+    public float rotateSpeed = 2f;
+
+    private Rigidbody agentRigidbody;
     private Vector3 startPos;
-    private bool dead = false;
+
     void Start()
     {
-        rBody = GetComponent<Rigidbody>();
+        agentRigidbody = GetComponent<Rigidbody>();
         startPos = transform.localPosition;
     }
 
     public Transform Target;
     public override void AgentReset()
     {
-        dead = false;
         transform.localPosition = startPos;
-        this.rBody.angularVelocity = Vector3.zero;
-        this.rBody.velocity = Vector3.zero;
+        this.agentRigidbody.angularVelocity = Vector3.zero;
+        this.agentRigidbody.velocity = Vector3.zero;
     }
 
     public override void CollectObservations()
@@ -28,80 +30,83 @@ public class MouseAgent : Agent
         AddVectorObs(Target.localPosition);
         AddVectorObs(this.transform.localPosition);
 
-        // Agent velocity
-        AddVectorObs(rBody.velocity.x);
-        AddVectorObs(rBody.velocity.z);
+        // Add velocity observation
+        Vector3 localVelocity = transform.InverseTransformDirection(agentRigidbody.velocity);
+        AddVectorObs(localVelocity.x);
+        AddVectorObs(localVelocity.z);
     }
 
     public float speed = .5f;
     public override void AgentAction(float[] vectorAction, string textAction)
     {
-        if (dead)
+        // Determine the rotation action
+        float rotateAmount = 0;
+        if (vectorAction[1] == 1)
         {
-            SetReward(-1f);
+            rotateAmount = -rotateSpeed;
+        }
+        else if (vectorAction[1] == 2)
+        {
+            rotateAmount = rotateSpeed;
+        }
+
+        // Apply the rotation
+        Vector3 rotateVector = transform.up * rotateAmount;
+        agentRigidbody.MoveRotation(Quaternion.Euler(agentRigidbody.rotation.eulerAngles + rotateVector * rotateSpeed));
+
+        // Determine move action
+        float moveAmount = 0;
+        if (vectorAction[0] == 1)
+        {
+            moveAmount = moveSpeed;
+        }
+        else if (vectorAction[0] == 2)
+        {
+            moveAmount = moveSpeed * -.5f; // move at half-speed going backwards
+        }
+
+        // Apply the movement
+        Vector3 moveVector = transform.forward * moveAmount;
+        agentRigidbody.AddForce(moveVector * moveSpeed, ForceMode.VelocityChange);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition,
+                                                  Target.localPosition);
+
+        // Getting closer to target
+        if (distanceToTarget > 1.42f)
+        {
+            AddReward(1.0f / distanceToTarget);
+        }
+        // Reached target
+        else if (distanceToTarget < 1.42f)
+        {
+            // Set rewards reset all previous rewards
+            // SetReward(1.0f);
+            AddReward(1.0f);
+            Done();
+        }
+
+        // Determine state
+        if (GetCumulativeReward() <= -5f)
+        {
+            // Reward is too negative, give up
             Done();
         }
         else
         {
-            SetReward(0.01f);
-            // Actions, size = 2
-            //Debug.LogFormat("ControlSignal: {0} ; {1}", controlSignal.x, controlSignal.z);
-
-            Vector3 controlSignal = Vector3.zero;
-            var forwardAxis = (int)vectorAction[0];
-            var rightAxis = (int)vectorAction[1];
-
-            switch (forwardAxis)
-            {
-                case 1:
-                    transform.Translate(transform.forward*speed);
-                    break;
-                case 2:
-                    transform.Translate(-transform.forward *speed);
-                    break;
-
-            }
-
-            switch (rightAxis)
-            {
-                case 1:
-                    transform.Translate(transform.right * speed);
-                    break;
-                case 2:
-                    transform.Translate(-transform.right * speed);
-                    break;
-
-            }
-            //controlSignal.x = vectorAction[0];
-            //controlSignal.z = vectorAction[1];
-            //rBody.AddForce(controlSignal * speed);
-
-            // Rewards
-            float distanceToTarget = Vector3.Distance(this.transform.localPosition,
-                                                      Target.localPosition);
-
-            // Getting closer to target
-            if (distanceToTarget > 1.42f)
-            {
-                SetReward(-0.001f * distanceToTarget);
-            }
-
-            // Reached target
-            if (distanceToTarget < 1.42f)
-            {
-                // Set rewards reset all previous rewards
-                // SetReward(1.0f);
-                SetReward(1.0f);
-                Done();
-            }
+            // Encourage movement with a tiny time penalty and pdate the score text display
+            AddReward(-.001f);
+            //agentArea.UpdateScore(GetCumulativeReward());
         }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("fence"))
         {
-            dead = true;
+            AddReward(-.01f);
         }
     }
 }
