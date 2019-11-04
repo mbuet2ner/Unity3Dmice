63a3c07a2268a07d89ed3566ce2b6c0a6b04ae56
mbuet2ner <20720363+mbuet2ner@users.noreply.github.com>
2019-11-04 17:40:37 +0200

    added missing mouse prefab and model

---
+++ AI Mice/Assets/Sphere.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class Sphere : Agent
+{
+    Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public Transform Target;
+    public override void AgentReset()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            // If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(-11f, 0.4f, -13f);
+        }
+    }
+
+    public override void CollectObservations()
+    {
+        // Target and Agent positions
+        AddVectorObs(Target.localPosition);
+        AddVectorObs(this.transform.localPosition);
+
+        // Agent velocity
+        AddVectorObs(rBody.velocity.x);
+        AddVectorObs(rBody.velocity.z);
+    }
+
+    public float speed = 10;
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * speed);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition,
+                                                  Target.localPosition);
+
+        // Reset when it takes to long
+
+
+        // Getting closer to target
+        //if (distanceToTarget > 1.42f)
+        //{
+        //    AddReward(-0.001f * distanceToTarget);
+        //}
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            // Set rewards reset all previous rewards
+            // SetReward(1.0f);
+            AddReward(1.0f);
+            Done();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("fence"))
+        {
+            AddReward(-0.1f);
+        }
+    }
+}
---
+++ AI Mice/Assets/SphereAgent.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class SphereAgent : Agent
+{
+    Rigidbody rBody;
+    private Vector3 startPos;
+    private bool dead = false;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        startPos = transform.localPosition;
+    }
+
+    public Transform Target;
+    public override void AgentReset()
+    {
+        dead = false;
+        transform.localPosition = startPos;
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+    }
+
+    public override void CollectObservations()
+    {
+        // Target and Agent positions
+        AddVectorObs(Target.localPosition);
+        AddVectorObs(this.transform.localPosition);
+
+        // Agent velocity
+        AddVectorObs(rBody.velocity.x);
+        AddVectorObs(rBody.velocity.z);
+    }
+
+    public float speed = 10;
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        if (dead)
+        {
+            SetReward(-1f);
+            Done();
+        }
+        else
+        {
+            SetReward(0.01f);
+            // Actions, size = 2
+            Vector3 controlSignal = Vector3.zero;
+            controlSignal.x = vectorAction[0];
+            controlSignal.z = vectorAction[1];
+            rBody.AddForce(controlSignal * speed);
+
+            // Rewards
+            float distanceToTarget = Vector3.Distance(this.transform.localPosition,
+                                                      Target.localPosition);
+
+            // Getting closer to target
+            if (distanceToTarget > 1.42f)
+            {
+                SetReward(-0.001f * distanceToTarget);
+            }
+            
+            // Reached target
+            if (distanceToTarget < 1.42f)
+            {
+                // Set rewards reset all previous rewards
+                // SetReward(1.0f);
+                SetReward(1.0f);
+                Done();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("fence"))
+        {
+            dead = true;
+        }
+    }
+}
