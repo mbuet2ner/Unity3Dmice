7adea1f3b6030a79f236cb03031658fa3bc909a8
ismailceler <ismailceler@googlemail.com>
2019-12-28 15:03:09 +0100

    WolfFOV

---
+++ AI Mice/Assets/Editor/FOVEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (WolfFOV))]
+public class FOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        WolfFOV wolf = (WolfFOV)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(wolf.transform.position, Vector3.up, Vector3.forward, 360, wolf.fovRadius);
+        Vector3 viewAngle1 = wolf.Direction(-wolf.fovAngel / 2, false);
+        Vector3 viewAngle2 = wolf.Direction(wolf.fovAngel / 2, false);
+
+        Handles.DrawLine(wolf.transform.position, wolf.transform.position + viewAngle1 * wolf.fovRadius);
+        Handles.DrawLine(wolf.transform.position, wolf.transform.position + viewAngle2 * wolf.fovRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTargets in wolf.mouseInView)
+        {
+            Handles.DrawLine(wolf.transform.position, visibleTargets.position);
+        }
+    }
+}
---
+++ AI Mice/Assets/WolfFOV.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfFOV : MonoBehaviour
+{
+    public float movSpeed;
+    public float fovRadius;
+    [Range(0, 360)]
+    public float fovAngel;
+
+    public LayerMask targetLayer;
+    public LayerMask obstLayer;
+
+    private Rigidbody wolfRb;
+    private Vector3 startPos;
+
+    public List<Transform> mouseInView = new List<Transform>();
+
+
+    float baseDirection = 0;
+
+
+
+
+    void Start()
+    {
+        StartCoroutine("FindMouseEnmu", .2f);
+        wolfRb = GetComponent<Rigidbody>();
+        startPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+     transform.position += transform.forward * movSpeed * Time.deltaTime;
+    }
+
+    IEnumerator FindMouseEnmu(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindMouse();
+        }
+    }
+
+    void FindMouse()
+    {
+        mouseInView.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, fovRadius, targetLayer);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform mouse = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (mouse.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < fovAngel / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, mouse.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstLayer))
+                {
+                    mouseInView.Add(mouse);
+                    transform.LookAt(mouseInView[0].position);
+                }
+            }
+        }
+    }
+
+
+    public Vector3 Direction(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "fence")
+        {
+            baseDirection = baseDirection + Random.Range(-30, 30);   // Switch to a new direction on collision
+
+            // use the above code as a template for all the collisionTags
+            // add here.. and on.. and on..
+        }
+        else if (col.gameObject.tag == "mouse")
+        {
+            transform.localPosition = startPos;
+
+        }
+    }
+}
